afc33a32b4c0662ba6d8dd24acb9eb5d6e0ede3d
msamuel25600 <96905399+msamuel25600@users.noreply.github.com>
2023-03-01 04:41:11 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+var loop = ["martin", 4];
+for (var i = 0; i < loop.length; i++) {
+  document.write(loop[i] + "</br>");
+}
+
+function addition() {
+  var numbers = [4, 6, 8, 9];
+  return numbers[0] + numbers[1] + numbers[2] + numbers[3];
+}
+document.write(addition() + "</br>");
+
+function evenNumbers(num) {
+  if (num % 2 === 0) {
+    document.write(num + "</br>");
+  }
+}
+
+var numbers = [2, 7, 4, 6, 12, 17];
+numbers.forEach(evenNumbers);
+
+function reverse() {
+  const number = [2, 7, 4, 6, 12, 17];
+  return number.reverse();
+}
+document.write(reverse() + "</br>");
+
+function largest() {
+  const numbers = [2, 7, 4, 6, 12, 17];
+  return Math.max(...numbers);
+}
+document.write(largest());
+
+export { evenNumbers, reverse, largest };
